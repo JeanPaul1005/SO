7dc341c4a3380c89cd40ce7e4e12460a1b38ac57
Jean Paul <jeanpaullmm1@gmail.com>
2022-07-08 23:19:33 -0500

    Actualziacion README

--- PC.cpp
+++ PC.cpp
@@ -5,56 +5,56 @@
 
 using namespace std;
 
-#define NUM_HILOS 10
+#define NUM_HILOS 5
 
-queue<char> cola;
+queue<char> buffer;
 const char letras[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-int cont=0;
 mutex flag;
 
+int cont = 0;
 class Monitor {
 	public:
 	void producir(int productor){
-		flag.lock();	// LOCK
+		flag.lock();	
 		int aleatorio;
 		aleatorio = rand() % 26;
 		char producido=letras[aleatorio];
-		cola.push(producido);
+		buffer.push(producido);
 		cout<<"El productor "<<productor<<" Está produciendo: "<<producido<<endl;
-		flag.unlock();	// UNLOCK		
+		flag.unlock();		
 	}
 
 			
 	void consumir(int consumidor){
-	    flag.lock();	// LOCK
-		int aleatorio;
+    flag.lock();	
+    int aleatorio;
 		aleatorio = rand() % 26;
 		char consumido=letras[aleatorio];
-		cola.pop();
-		cout<<"El consumidor "<<consumidor<<"Está consumiendo:"<<consumido<<endl;
-		flag.unlock();	// UNLOCK		
+		buffer.pop();
+		cout<<"El consumidor "<<consumidor<<"Está consumiendo: "<<consumido<<endl;
+		flag.unlock();		
 	}
 };
 
 class Productor {
 	private:
 		Monitor* monitor;
-		thread t;
-		int id;
+		thread hilo;
+		int nombre;
 		void run_thread(){			
 			for (int i=1; true; i++) {
-			    monitor->producir(id);		//	RUN
-			}
+			    monitor->producir(nombre);		
 		}
+      }
 			
 	public:
-		Productor(Monitor* mon, int idd){
-			id = idd;
-			monitor = mon;
-			t = thread(&Productor::run_thread, this);
+		Productor(Monitor* _monitor, int _nombre){
+			nombre = _nombre;
+			monitor = _monitor;
+			hilo = thread(&Productor::run_thread, this);
 		}
 		void join_thread(){
-			t.join();	// START
+			hilo.join();	
 		}
 		
 };
@@ -62,22 +62,42 @@ class Productor {
 class Consumidor {
 	private:
 		Monitor* monitor;
-		thread t;
-		int id;
+		thread hilo;
+		int nombre;
 		void run_thread(){			
 			for (int i=1; true; i++) {
-			    monitor->consumir(id);		//	RUN
+			    monitor->consumir(nombre);		
 			}
 		}
 			
 	public:
-		Consumidor(Monitor* mon, int idd){
-			id = idd;
-			monitor = mon;
-			t = thread(&Consumidor::run_thread, this);
+		Consumidor(Monitor* _monitor, int _nombre){
+			nombre = _nombre;
+			monitor = _monitor;
+			hilo = thread(&Consumidor::run_thread, this);
 		}
 		void join_thread(){
-			t.join();	// START
+			hilo.join();	
 		}
 		
 };
+
+
+int main() {
+
+	Productor* productor[NUM_HILOS];
+	Consumidor* consumidor[NUM_HILOS];
+	Monitor* monitor;
+	
+	for(int i=0; i<NUM_HILOS; i++) {
+		productor[i] = new Productor(monitor, i);
+		consumidor[i] = new Consumidor(monitor, i);
+	}
+	
+	for(int i=0; i<NUM_HILOS; i++) {
+		productor[i]->join_thread();	//	START
+	    consumidor[i]->join_thread();	//	START
+	}
+
+	return 0;
+}
